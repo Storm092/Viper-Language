33b634cf0121270a26ce0797911b26bf16f419bd
Storm <92469024+Storm092@users.noreply.github.com>
2021-10-16 21:19:03 +0200

    Added delay func. make sure to recompile

--- ViperL/ViperL/Program.cs
+++ ViperL/ViperL/Program.cs
@@ -1,10 +1,11 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ViperL
@@ -90,6 +91,13 @@ namespace ViperL
                             {
                                 string msg = line.Replace("Print >", "");
                                 Console.WriteLine(msg);
+                            }
+                            if (line.StartsWith("Delay > "))
+                            {
+                                string time = line.Replace("Delay > ", "");
+                                string ms = time;
+                                int timeI = Convert.ToInt32(ms);
+                                Thread.Sleep(timeI);
                             }
                             if (line.StartsWith("PauseConsole"))
                             {
